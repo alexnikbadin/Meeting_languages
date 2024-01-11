a4db36875c5e7e127ed15fc3f81d13ba0c69559f
Alex.B <badinalexander@icloud.com>
2024-01-11 18:23:03 +0300

    create massive

---
+++ Lection_2/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[5];
+arr[0] = 4;
+arr[1] = 5;
+arr[2] = 17;
+arr[3] = 8;
+arr[4] = 90;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = { 1, 2, 3, 4, 5 };//more simple method
+
+
